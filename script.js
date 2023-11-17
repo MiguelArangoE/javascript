dc6316149e70e14abe5389a216846645b6e836ea
MiguelArangoE <miguel.arango.business@gmail.com>
2023-11-16 22:04:32 -0500

    Última actualización del día

--- script.js
+++ script.js
@@ -14,36 +14,37 @@
 //     dataSet
 // });
 
-const username = prompt("Bienvenido al portal de ejercicios JS, por favor ingrese su usuario:")
-
+const username = prompt("Bienvenido al portal de ejercicios JS, por favor ingrese su usuario:");
 
 function login(argumentos) {
     console.log(argumentos)
     console.log(typeof argumentos.user)
     switch (true) {
         case (argumentos.user == "admin" && argumentos.password == "123"):
-            return true
-        case argumentos.user !== null && argumentos.user.length > 3:
-            return true
+            return true;
+        case argumentos.user !== null && argumentos.user.length > 0:
+            return true;
         case (argumentos.user == null):
-            alert("Bienvenido usuario desconocido")
+            alert("Bienvenido usuario desconocido");
         default:
-            alert("Usuario / contraseña incorrectos.")
+            alert("Usuario / contraseña incorrectos.");
             break;
     }
-}
+};
 
-// et info_test = ['miguel', '123']
+// let prueba_de_datos_en_tabla = ['miguel', '123']
+// console.log(typeof prueba_de_datos_en_tabla)
 
-let login_info = {
+let login_info = { // datos en diccionario
     user: username,
     password: 123,
 };
+console.log(typeof login_info);
 
 if (login(login_info)) {
     let notificacion = "Bienvenido, " + login_info.user + "!"
     alert(notificacion)
     document.write('<h1>' + notificacion + '</h1>')
-    // comentarioss
-}
+    // Comentarioss
+};
 
